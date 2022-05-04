50a5d4b6d33c8f9387290717492966af38a41e96
katzworld <kstewartjr77@gmail.com>
2022-05-04 02:00:07 -0400

    no buzz // clean up code check comment

--- walking.js
+++ walking.js
@@ -5,16 +5,12 @@
 // @description  mash up somethings to make a thing to show things over things with the other things and who owned them things
 // @author       @KaTZWorlD find me on twitter or in the discord
 // @match        http*://play.tmwstw.io/*
-<<<<<<< HEAD
 // @require      file://
-=======
->>>>>>> 8f994aa8d6019b2f5942ad45c64c2af295a23d40
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @icon         https://avatars.githubusercontent.com/u/3157472
 // @grant        none
 // ==/UserScript==
 
-<<<<<<< HEAD
 // Your code here... dont know code  sorry  ?
 jQuery(document).ready(function() {
     jQuery('*:contains("SIGN"):not(:has(*))').css('background-color', 'green').click(function() {
@@ -82,48 +78,3 @@ jQuery(document).ready(function() {
     //'use strict';
     //still nothing here WTF?!
 })();
-=======
-    // Your code here... dont know code  sorry  ?
-
-
-
-let p = jQuery(".plot_id") // plot got tired of typing it P it is !
-let globe = 'https://claim.tmwstw.io/data/previews/'
-let apiUrl ='https://api.tmwstw.io/'
-
-p.on('DOMSubtreeModified', function() { callsToApi(p); })
-
-function callsToApi(p){
-  jQuery('#plot_id').hide()// we display it on the img in the katbox now
-  jQuery('#theImg').hide() //hide the globe before update just like houdini
-  makeTheGlobe(p) //where are we 
-  whoOwnThis(p) //who owns this ?
-}
-
-
-function whoOwnThis(p) {
-    jQuery('#katbox').remove();  //clean the liter box
-   this.plot = p.text().split('#')[1] //just the number
-//grab that put a new elem with the owner info above the globe 
- let whoFromApi = new URL('/owner_of_plot=' + this.plot, apiUrl)
- fetch(whoFromApi)
-    .then(response => response.text())
-    .then(data => jQuery('#theImg').before('<h3 class="side_note_txt" id="katbox">' + data.toString())) 
-   }
-
-
-function makeTheGlobe(p) { //get the p from the other function
-    this.plot = p.text().split('#')[1] //just the number
-    let globeThing = new URL(this.plot + ".jpg", globe); //make the URL
-    jQuery('#side_notes').prepend(jQuery('<img>', { id: 'theImg', src: globeThing.href })) //add that into the sides notes spot
-    jQuery('#theImg').width(200) //dress it up
-    jQuery('#theImg').height(200) //change this ?
-}
-
-(function() {
-    'use strict';
-    //still nothing here WTF?! 
-})();
-
-
->>>>>>> 8f994aa8d6019b2f5942ad45c64c2af295a23d40
